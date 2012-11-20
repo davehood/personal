3721e584518b442bf6f59aebdf5940681908ea03
Dave Hood <davehood@gmail.com>
2012-11-20 15:48:03 -0500

    blah blah

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var http = require("http");
 http.createServer( function(req, res) {
     
     res.writeHead( 200, {'Content-Type': 'text/plain'} );
-    res.end("Dave's first web server");
+    res.end("Welcome to Dave's first fully cloud-capable app " +
+        "written in highly-scalable, non-blocking, asynchronous server-side JavaScript (Node.js) " +
+        "using a cloud-based IDE (Cloud9) " +
+        "with a cloud-based version control system (GitHub) " +
+        "running on a cloud Platform as a Service (Heroku) ");
     
 }).listen(process.env.PORT, process.env.IP);
 
